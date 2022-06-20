f923ebb81b7daf0e77ff9386c93a6d422b5e8748
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-06-20 14:52:45 +0300

    added cryptocoin flatlist

---
+++ src/components/PortfolioCoin/styles.ts
@@ -0,0 +1,20 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+   root:{
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    backgroundColor: 'red',
+    height: 100,
+    margin: 10,
+   },
+   image:{
+    height: 50,
+    width: 50,
+   },
+   left: {
+    flexDirection: 'row'
+   }
+});
+
+export default styles;
--- src/screens/PortfolioScreen/index.tsx
+++ src/screens/PortfolioScreen/index.tsx
@@ -1,10 +1,47 @@
-import { View, Text } from 'react-native'
+import { View, Text, Image, FlatList } from 'react-native'
 import React from 'react'
+import styles from './styles'
+import PortfolioCoin from '../../components/PortfolioCoin/index'
 
+const image = require( '../../../assets/images/Saly-10.png');
+
+const portfolioCoins = [{
+    id: '1',
+    image:'https://cdn-icons-png.flaticon.com/512/25/25228.png',
+    name: 'Virtual Dollars',
+    symbol: 'USD',
+    amount: 69.420,
+    valueUSD: 69.420
+},
+{
+    id: '2',
+    image:'https://bitcoin.org/img/icons/opengraph.png?1652976465',
+    name: 'Bitcoin',
+    symbol: 'BTC',
+    amount: 1.13,
+    valueUSD: 69.420
+},
+{
+    id: '3',
+    image:'https://upload.wikimedia.org/wikipedia/commons/thumb/0/01/Ethereum_logo_translucent.svg/1200px-Ethereum_logo_translucent.svg.png',
+    name: 'Ethereum',
+    symbol: 'ETH',
+    amount: 30,
+    valueUSD: 30.420
+}]
 const PortfolioScreen = () => {
     return (
-        <View  style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-            <Text> Portfolio Screen</Text>
+        <View  style={styles.root}>
+            <Image style={styles.image} source={image}/>
+            <View style={styles.balanceContainer}>
+                <Text style={styles.label}> Portofolio Balance</Text>
+                <Text style={styles.balance}>$69.420</Text>
+            </View>
+            <FlatList
+                style={{ width: '100%', height: 50}}
+                data={portfolioCoins}
+                renderItem={({item}) => <PortfolioCoin portfolioCoin={item}/>}
+            />
         </View>
     )
 }
