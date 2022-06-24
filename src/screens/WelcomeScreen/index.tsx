d0b2fc94c25765d4a64fd59138089e6f768aaf87
Collins Koech <35452377+collinskoech11@users.noreply.github.com>
2022-06-24 19:39:06 +0300

    sample navigation

--- src/screens/WelcomeScreen/index.tsx
+++ src/screens/WelcomeScreen/index.tsx
@@ -1,5 +1,6 @@
-import { View, Text, Image, Pressable } from 'react-native'
+import { View, Text, Image, Pressable, Alert } from 'react-native'
 import React from 'react'
+import {useNavigation} from '@react-navigation/native'
 import styles from './styles'
 const image = require( '../../../assets/images/Saly-1.png' );
 const googlebuttonimage = require( '../../../assets/images/google-button.png' );
@@ -7,17 +8,18 @@ const applebuttonimage = require( '../../../assets/images/apple-button.png' );
 
 const WelcomeScreen = () => {
     const signInGoogle = () => {
-
+        console.warn('Learning React Native')
     }
+    const navigation = useNavigation();
     return (
         <View  style={styles.root}>
             <Image style={styles.image} source={image} />
             <Text style={styles.header1}>Welcome Screen to VCRYPTO</Text>
             <Text style={styles.header2}>Invest your virtual $100000 and compete woth others</Text>
-            <Pressable style={styles.buttonImageAppleContainer} onPress={signInGoogle}>
+            <Pressable style={styles.buttonImageAppleContainer} onPress={() => navigation.navigate('Root')}>
                 <Image style={styles.buttonImage}  source={googlebuttonimage}/>
             </Pressable>
-            <Pressable style={styles.buttonImageContainer} onPress={signInGoogle}>
+            <Pressable style={styles.buttonImageContainer} onPress={() => navigation.navigate('Root')}>
                 <Image style={styles.buttonImage}  source={applebuttonimage}/>
             </Pressable>
         </View>
